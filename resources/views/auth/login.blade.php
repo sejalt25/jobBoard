8255799252aef734ce48784a8967ff06e8afcdf6
sejal <sejalt25@gmail.com>
2024-07-29 21:25:37 +0530

    jobBoard First commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,170 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="section-hero overlay inner-page bg-image" style="margin-top: -24px; background-image: url({{asset('assets\images\hero_1.jpg')}}); margin-top: -24px;" id="home-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <h1 class="text-white font-weight-bold">Log In</h1>
+            <div class="custom-breadcrumbs">
+              <a href="#">Home</a> <span class="mx-2 slash">/</span>
+              <span class="text-white"><strong>Log In</strong></span>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
+<div class="container">
+    <div class="row justify-content-center">
+  <div class="container">
+  <div class="row align-items-center justify-content-center">
+    <div class="col-md-12">
+      <div class="mb-5 text-center">
+        <h1 class="text-white font-weight-bold">The Easiest Way To Get Your Dream Job</h1>
+        <!-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate est, consequuntur perferendis.</p> -->
+      </div>
+      <!-- <form method="post" class="search-jobs-form">
+        <div class="row mb-5">
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <input type="text" class="form-control form-control-lg" placeholder="Job title, Company...">
+          </div>
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <select class="selectpicker" data-style="btn-white btn-lg" data-width="100%" data-live-search="true" title="Select Region">
+              <option>Anywhere</option>
+              <option>San Francisco</option>
+              <option>Palo Alto</option>
+              <option>New York</option>
+              <option>Manhattan</option>
+              <option>Ontario</option>
+              <option>Toronto</option>
+              <option>Kansas</option>
+              <option>Mountain View</option>
+            </select>
+          </div>
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <select class="selectpicker" data-style="btn-white btn-lg" data-width="100%" data-live-search="true" title="Select Job Type">
+              <option>Part Time</option>
+              <option>Full Time</option>
+            </select>
+          </div>
+          <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <button type="submit" class="btn btn-primary btn-lg btn-block text-white btn-search"><span class="icon-search icon mr-2"></span>Search Job</button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 popular-keywords">
+            <h3>Trending Keywords:</h3>
+            <ul class="keywords list-unstyled m-0 p-0">
+              <li><a href="#" class="">UI Designer</a></li>
+              <li><a href="#" class="">Python</a></li>
+              <li><a href="#" class="">Developer</a></li>
+            </ul>
+          </div>
+        </div>
+      </form> -->
+    </div>
+  </div>
+</div>
+
+<a href="#next" class="scroll-button smoothscroll">
+  <span class=" icon-keyboard_arrow_down"></span>
+</a>
+
+</section>
+        <div class="col-md-12">
+        <form style="margin-top: -100px;" action="{{ route('login') }}" method="POST" class="p-4 border rounded">
+          @csrf
+            <div class="row form-group">
+              <div class="col-md-12 mb-3 mb-md-0">
+                <label class="text-black" for="fname">Email</label>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                  @error('email')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+              </div>
+            </div>
+            <div class="row form-group mb-4">
+              <div class="col-md-12 mb-3 mb-md-0">
+                <label class="text-black" for="fname">Password</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                  @error('password')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+              </div>
+            </div>
+            <div class="row form-group">
+              <div class="col-md-12">
+                <input type="submit" name="submit" value="Log In" class="btn px-4 btn-primary text-white">
+              </div>
+            </div>
+          </form>
+            <!-- <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</div>
+@endsection
